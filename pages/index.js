f36f5d0b36888098ecd5f5def1d972cc07dd0661
Arman-Kabir <armankabir8@gmail.com>
2023-04-11 01:31:26 +0600

    simple html added

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="home">
-      
+      <div className="nav bg-gray-400 text-red-600">
+        I am navbar
+      </div>
+      <span>I am homepage</span>
     </div>
   )
 }
